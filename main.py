86fd16facbcadeea270fffd9b67844f5db89cfa1
lasithpalihawadana <lasithpalihawadana@gmail.com>
2024-05-23 16:27:18 +0530

    Add New Scanning Type

--- main.py
+++ main.py
@@ -4,56 +4,73 @@ from scapy.layers.inet import IP, TCP, ICMP
 from tabulate import tabulate
 
 
-print("------SCAPY PORT SCANNER------")
+print("--------------SCAPY PORT SCANNER------")
+print("""\
+     Press 1: Scan a Specific IP Address with a Specific Port
+     Press 2: Scan an IP Address with a Port Range
+     Press 3: Scan a Network for Open Ports within a Port Range""")
+print("--------------------")
 
-def is_up(ip):
-    icmp=IP(dst=ip)/ICMP()
-    resp=sr1(icmp,timeout=10)
-    if resp==None:
-        getLogger("Server is Down")
-        print("Server is Down")
-        return False
-    else:
-        getLogger("Server is Up an Running")
-        print("Server is Up")
+def is_ip_reachable(ip):
+    icmp_packet = IP(dst=ip)/ICMP()
+    response = sr1(icmp_packet, timeout=5, verbose=0)
+    if response:
         return True
-
-
-def sendPacket(pkt):
-    if pkt:
-     if pkt.haslayer(TCP):
-        tcp_layer = pkt.getlayer(TCP)
-        print(f"Source Port: {tcp_layer.sport}")
-        print(f"Source Port: {tcp_layer.sport}")
-        print(f"Destination Port: {tcp_layer.dport}")
-        print(f"Flags: {tcp_layer.flags}")
-        print(f"Sequence Number: {tcp_layer.seq}")
-        print(f"Acknowledgment Number: {tcp_layer.ack}")
-
-     if pkt.haslayer(IP):
-        ip_layer = pkt.getlayer(IP)
-        print(f"Source IP: {ip_layer.src}")
-        print(f"Destination IP: {ip_layer.dst}")
-        print(f"TTL: {ip_layer.ttl}")
     else:
-     print("No response received.")
+        return False
 
 def sendRequest():
-    userinput2=input("Enter Target IP Address")
-    userinput3=int(input("Enter Port Number"))
-    return sr1(IP(dst=userinput2)/TCP(dport=userinput3,flags="S"), timeout=4, verbose=0)
+    userinput=input("Type:")
+    if userinput=="1":
+       ipaddress=input("Enter Target IP Address")
+       if is_ip_reachable(ipaddress):
+          userinput3=int(input("Enter Target Port Number"))
+          return sendPacket(ipaddress,userinput3)
+       else:
+           print("IP IS Not Reachable")
+           return False
+    if userinput=="2":
+       ipaddress=input("Enter Target IP Address")
+       if is_ip_reachable(ipaddress):
+           minport=int(input("Enter Min Port Number"))
+           maxport=int(input("Enter Max Port Number"))
+           return list_open_ports(ipaddress,minport,maxport)
+       else:
+           print("IP IS Not Reachable")
+           return False
+    else:
+       return False
 
-sendPacket(sendRequest())
+def sendPacket(ipaddress, port):
+    port_status={}
+    pkt = sr1(IP(dst=ipaddress)/TCP(dport=port, flags="S"), timeout=4, verbose=0)
+    if pkt and pkt.haslayer(TCP):
+            tcp_layer = pkt.getlayer(TCP)
+            if tcp_layer.flags == "SA":
+                port_status[port] = "Port Open"
+            elif tcp_layer.flags == "RA":
+                port_status[port] = "Port Close"
+    else:
+        port_status[port] = "Port Close"
+    return port_status
 
-data = [
-    ["Source Port", 8989],
-    ["Destination Port", 20],
-    ["Flags", "RA"],
-    ["Sequence Number", 0],
-    ["Acknowledgment Number", 1],
-    ["Source IP", "45.79.112.203"],
-    ["Destination IP", "192.168.8.152"],
-    ["TTL", 42]
-]
-# Display the table
-print(tabulate(data, headers=["Field", "Value"], tablefmt="plain"))
+def list_open_ports(ip,minport,maxport):
+    port_status = {}
+    for port in range(minport, maxport):
+        pkt = sr1(IP(dst=ip)/TCP(dport=port, flags="S"), timeout=4, verbose=0)
+        if pkt and pkt.haslayer(TCP):
+            tcp_layer = pkt.getlayer(TCP)
+            if tcp_layer.flags == "SA":
+               port_status[port] = "Port Open"
+            else:
+                port_status[port] = "Port Close"
+    return port_status
+
+
+
+
+datalist=sendRequest()
+data = [[port, status] for port, status in datalist.items()]
+
+print(tabulate(data,headers=["Port","Status"], tablefmt="plain"))
+
